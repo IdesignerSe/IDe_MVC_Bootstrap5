1f2297a343b1f1c1e69df1210d0990a96f9c5572
idesignerse <heberandrade@gmail.com>
2021-04-18 14:01:02 +0200

    Starter

---
+++ IDe_MVC_Bootsrap5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IDe_MVC_Bootsrap5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
